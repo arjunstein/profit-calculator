dc4da76b81267342cd2eafee2784406846b8c0b8
Arjunstein <arjun.gnw09@gmail.com>
2024-11-26 09:26:28 +0700

    ✨ implement function

--- main.go
+++ main.go
@@ -3,31 +3,28 @@ package main
 import "fmt"
 
 func main()  {
-	var revenue float64
-	var expense float64
-	const taxRate float64 = 11
 
-	fmt.Print("Pemasukan Rp. ")
-	fmt.Scan(&revenue)
-	fmt.Print("Modal Rp. ")
-	fmt.Scan(&expense)
-	fmt.Print(`Pajak 11% = `, taxRate)	
+	revenue := getInputUser("Pemasukan Rp. ")
+	expense := getInputUser("Modal Rp. ")
+	taxRate := getInputUser("Pajak 11% ")
 
-	earningBeforeTax := revenue - expense
-	earningAfterTax := earningBeforeTax - (earningBeforeTax * taxRate / 100)
-	ratio := (earningBeforeTax / revenue) * 100
-	ppn := earningBeforeTax - earningAfterTax
+	ebt, profit, rasio := calculateFinancials(revenue,expense,taxRate)
 
+	fmt.Println("EBT : Rp.", ebt)
+	fmt.Println("Profit : Rp.", profit)
+	fmt.Println("Rasio Profit : ", rasio)		
+}
+
+func calculateFinancials(revenue, expense, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expense
 	profit := ebt * (1 - taxRate / 100)
 	rasio := ebt / profit
+	return ebt, profit, rasio
+}
 
-	fmt.Println("\nLaba Sebelum Pajak : Rp.", earningBeforeTax)
-	fmt.Println("Laba Setelah Pajak : Rp.", earningAfterTax)
-	fmt.Println("PPn : Rp.", ppn)
-	fmt.Println("Rasio Laba : ", ratio, "%")
-	fmt.Println("EBT : Rp.", ebt)
-	fmt.Println("Profit : Rp.", profit)
-	fmt.Println("Rasio Profit : ", rasio)	
-	
+func getInputUser(infotext string) float64 {
+	var userInput float64
+	fmt.Print(infotext)
+	fmt.Scan(&userInput)
+	return userInput
 }
