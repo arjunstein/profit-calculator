a7e3b1e9a21834855a856e7566523a5b8685141a
Arjunstein <arjun.gnw09@gmail.com>
2024-12-02 21:50:50 +0700

    "Added error handling to user input, stored results to file, and updated imports"

--- main.go
+++ main.go
@@ -1,18 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main()  {
 
-	revenue := getInputUser("Pemasukan Rp. ")
-	expense := getInputUser("Modal Rp. ")
-	taxRate := getInputUser("Pajak 11% ")
+	revenue, err := getInputUser("Pemasukan Rp. ")
+	if err != nil {
+		fmt.Println(err)
+		return
+		// panic(err)
+	}
+
+	expense, err := getInputUser("Modal Rp. ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	taxRate, err := getInputUser("Pajak 11% ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	ebt, profit, rasio := calculateFinancials(revenue,expense,taxRate)
 
 	fmt.Println("EBT : Rp.", ebt)
 	fmt.Println("Profit : Rp.", profit)
 	fmt.Println("Rasio Profit : ", rasio)		
+
+	storeResults(ebt, profit, rasio)
+}
+
+func storeResults(ebt, profit, rasio float64)  {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRasio: %.3f\n", ebt, profit, rasio)
+	os.WriteFile("results.txt", []byte(results), 0644)
 }
 
 func calculateFinancials(revenue, expense, taxRate float64) (float64, float64, float64) {
@@ -22,9 +47,14 @@ func calculateFinancials(revenue, expense, taxRate float64) (float64, float64, f
 	return ebt, profit, rasio
 }
 
-func getInputUser(infotext string) float64 {
+func getInputUser(infotext string) (float64, error) {
 	var userInput float64
 	fmt.Print(infotext)
 	fmt.Scan(&userInput)
-	return userInput
+
+	if userInput <= 0 {
+		return 0, errors.New("Input tidak boleh kurang dari 0")
+	}
+
+	return userInput, nil
 }
